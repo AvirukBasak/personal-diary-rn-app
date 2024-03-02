0e8b20208d9e9675e767733f1c13cf6efb9ecfc1
AvirukBasak <avirukbasak@gmail.com>
2024-03-03 03:00:35 +0530

    Add geolocation view and implement different views for different file types

--- src/pages/DiaryEntryPage/index.tsx
+++ src/pages/DiaryEntryPage/index.tsx
@@ -4,14 +4,79 @@ DiaryEntryPage structure
 - TitleBar
 - ScrollView containing the items of entryContent
 
+Views
+
+Each views takes base64 encoded data and displays it in a different way
+for text, it is a TextInput
+for geolocation, it is a MapView
+for picture, it is an Image
+for video, it is a Video
+
+on click, geolocation opens in map using an URI
+
+Image and Video are placed in a container that has a button to collapse the view
+
+For now, only text, geo and picture are implemented
+
 */
 
 import React, {useContext} from 'react';
-import {View, Text, StyleSheet, ScrollView} from 'react-native';
+import {View, StyleSheet, ScrollView, TextInput, Image} from 'react-native';
 import TitleBar from '../../components/Common/TitleBar';
+import ToolBar from '../../components/DiaryEntryPage/ToolBar';
 import DiaryEntryContextProvider, {
   DiaryEntryContext,
 } from '../../contexts/DiaryEntryContext';
+import {FileType} from '../../utis/documentFileSystem';
+
+function EditTextView({children}: {children: string}) {
+  return (
+    <TextInput
+      multiline
+      editable={false}
+      value={children}
+      style={{borderWidth: 1, borderColor: 'black'}}
+    />
+  );
+}
+
+function GeoView({children}: {children: string}) {
+  children = children.replace(' ', '+');
+  return (
+    <Image
+      source={{
+        uri: `https://www.google.com/maps/search/?api=1&query=${children}`,
+      }}
+      style={{width: 100, height: 100}}
+    />
+  );
+}
+
+function ImageView({children}: {children: string}) {
+  return (
+    <View>
+      <Image
+        source={{
+          uri: `data:image/png;base64,${children}`,
+        }}
+        style={{width: 100, height: 100}}
+      />
+    </View>
+  );
+}
+
+function VideoView({children}: {children: string}) {
+  return (
+    <View>
+      <Image
+        source={{
+          uri: `data:image/png;base64,${children}`,
+        }}
+        style={{width: 100, height: 100}}
+      />
+    </View>
+  );
+}
 
 function DiaryEntryPage() {
   const {
@@ -31,7 +96,18 @@ function DiaryEntryPage() {
         {entryContent.map((item, index) => {
           return (
             <View key={index}>
-              <Text>{item.content}</Text>
+              {item.extension === FileType.TEXT && (
+                <EditTextView>{item.content}</EditTextView>
+              )}
+              {item.extension === FileType.GEO && (
+                <GeoView>{item.content}</GeoView>
+              )}
+              {item.extension === FileType.IMAGE && (
+                <ImageView>{item.content}</ImageView>
+              )}
+              {item.extension === FileType.VIDEO && (
+                <VideoView>{item.content}</VideoView>
+              )}
             </View>
           );
         })}
