d9b6fa17bc7e574122445f4d60002ff11a89fd1c
AvirukBasak <avirukbasak@gmail.com>
2024-03-05 22:26:41 +0530

    Refactor HomePage component to display diary entries and handle empty diary case

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -8,35 +8,55 @@ HomePage structure
 
 import React from 'react';
 import {useEffect, useState} from 'react';
-import {View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet} from 'react-native';
 import TitleBar from '../../components/Common/TitleBar';
-import DiaryEntry from '../../components/HomePage/DiaryEntry';
+import DiaryEntry, {DiaryEntryType} from '../../components/HomePage/DiaryEntry';
 import AddNewEntryButton from '../../components/HomePage/AddNewEntryButton';
 import keyValueStorage from '../../utis/keyValueStorage';
 import logger from '../../utis/logger';
+import colors from '../../styles/colors';
 
 export default function HomePage() {
-  const [diaryEntries, setDiaryEntries] = useState<string[]>([]);
+  const [diaryEntries, setDiaryEntries] = useState<DiaryEntryType[]>([]);
   const [userName, setUserName] = useState('');
 
   useEffect(() => {
-    keyValueStorage.getAllDiaryEntries().then(setDiaryEntries);
+    keyValueStorage.getAllDiaryEntries().then(entires => {
+      const parsedEntries = entires.map(entry => JSON.parse(entry));
+      setDiaryEntries(parsedEntries);
+    });
+
     keyValueStorage
       .getValue('userName')
       .then(value => setUserName(value || 'User'));
-    logger.log('diary entries', diaryEntries);
-  }, [diaryEntries]);
+  }, []);
 
   useEffect(() => {
     keyValueStorage.setValue('userName', userName);
   }, [userName]);
 
+  useEffect(() => {
+    logger.log('loaded: userName:', userName);
+  }, [userName]);
+
+  useEffect(() => {
+    logger.log('loaded: diaryEntries:', diaryEntries);
+  }, [diaryEntries]);
+
   return (
     <View style={styles.container}>
       <TitleBar title={`Hello, ${userName}`} setTitle={setUserName} />
-      {diaryEntries.map((entry, index) => (
-        <DiaryEntry key={index} entry={JSON.parse(entry)} />
-      ))}
+      {diaryEntries.length === 0 ? (
+        <View style={styles.emptyDiary}>
+          <Text style={styles.emptyDiaryText}>
+            Start your diary by adding a new entry
+          </Text>
+        </View>
+      ) : (
+        diaryEntries.map((entry, index) => (
+          <DiaryEntry key={index} entry={entry} />
+        ))
+      )}
       <AddNewEntryButton />
     </View>
   );
@@ -45,6 +65,12 @@ export default function HomePage() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
+    backgroundColor: colors.primaryBgColor,
   },
+  emptyDiary: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  emptyDiaryText: {color: 'gray'},
 });
